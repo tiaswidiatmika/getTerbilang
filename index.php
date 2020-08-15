14b3d3a6e3a4a87ae5c94d362be3b3ba326e9fcf
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-15 16:49:20 +0600

    mengganti algoritma dengan pembagian

--- index.php
+++ index.php
@@ -1,131 +1,76 @@
 <?php
-    $ex = 23111;
+    $ex = 199099;
     function getTerbilang($number){
         $pivot = '';
         $positition = 0;
         $level = strlen(strval($number));
         $stringNumber = strval($number);
         $determinant = [
-            '0' => '',
-            '1' => 'satu',
-            '2' => 'dua',
-            '3' => 'tiga',
-            '4' => 'empat',
-            '5' => 'lima',
-            '6' => 'enam',
-            '7' => 'tujuh',
-            '8' => 'delapan',
-            '9' => 'sembilan',
-            '10' => 'puluh',
-            '11' => 'belas',
-            '12' => 'ratus',
-            '13' => 'ribu' 
+            0 => '',
+            1 => 'satu',
+            2 => 'dua',
+            3 => 'tiga',
+            4 => 'empat',
+            5 => 'lima',
+            6 => 'enam',
+            7 => 'tujuh',
+            8 => 'delapan',
+            9 => 'sembilan',
+            10 => 'puluh',
+            11 => 'sebelas',
+            12 => 'ratus',
+            13 => 'ribu' 
         ];
         
         
-        for ($i = $level; $i >= 0 ; $i--) {
-            $level = strlen(strval($number));
-            if ($level == 2){
-                // $stringNumber = strval($number);
-                // $level = strlen($stringNumber);
-                if ($stringNumber[0] == '1'){
-                    if ($stringNumber[1] == '1'){
-                        $pivot = $pivot . 'se' . $determinant[11];
-                        // return paksa pivot jika ternyata dia udah level belasan
-                        return $pivot;
+        if($number < 12){
+            // cek apa die ade di antare 0-11
+            return $pivot = $pivot . ' ' . $determinant[$number];
+        }elseif ($number > 11 && $number < 20 ) {
+            // jika lebih dari 11, maka cek  untuk belasan
+            return $pivot = $pivot . ' ' . ($determinant[$number-10]) . ' belas';
+        }elseif ($number > 19 && $number < 100) {
+            // untuk mengecek apakah dia memerlukan rekursi lagi untuk level puluhan
+            if ($number % 10 == 0) { // ini untuk ngecek yang habis pas tanpa sisa
+                return $pivot = $pivot . ' ' . $determinant[$number/10] . ' puluh';
+            }else {
+                // sedangkan baris ini untuk mengecek, apakah dia perlu rekursi lagi
+                return $pivot = $pivot . ' ' . $determinant[$number/10] . ' puluh' . getTerbilang($number % 10);
+            }     
+        }elseif ($number > 99 && $number < 1000) {
+            // belum bisa memasukkan seratusan biar dibaca seratus
+            if ($number / 100 > 0) {
+                if ($number / 100 == 1) { return $pivot = $pivot . ' ' . ' seratus'; }
+                return $pivot = $pivot . ' ' . getTerbilang($number/100) . ' ratus' . getTerbilang($number % 100);
+            }elseif ($number > 100 && $number % 100 == 0 ) {
+                return $pivot = $pivot . ' ' . getTerbilang($number%100) . ' ratus';
+            }else {
+                return $pivot = $pivot . ' ' . getTerbilang($number % 100);
+            }
+        }elseif ($number > 999 && $number / 1000 < 12) {
+            
+            if ($number / 1000 > 0) {
+                if ($number >= 1000 && $number <= 1999) {
+                    $pivot = $pivot . ' ' . ' seribu';
+                    if ($number % 1000 > 0){
+                        return $pivot = $pivot . ' ' . getTerbilang($number%1000);
                     }else {
-                        if ($determinant[strval($stringNumber[0])] == '0'){
-                            
-                            $pivot = $pivot . ' ' . $determinant[$stringNumber[0]] . ' ';
-                        }
-                        // return $stringNumber;
-                        $pivot = $pivot . $determinant[strval($stringNumber[1])] . $determinant[11];
-                        return $pivot;
+                        return $pivot = $pivot . ' ' . getTerbilang($number/1000) . ' ribu' . getTerbilang($number % 100);
                     }
                 }
-            }
-
-            if ($level == 3){
-                // $stringNumber = strval($number);
-                if ($stringNumber[0] == '1') {
-                    $pivot = $pivot . 'se' . $determinant[12];
-                    // mengupdate kondisi sekarang, mengkonversi 00 menjadi integer aslinya
-                    $number = intval($stringNumber);
-                    $stringNumber = strval($number);
-                }else {
-                    $pivot = $pivot . $determinant[strval($stringNumber[0])] . ' ' . $determinant[12] . ' ';
-                    $stringNumber = substr($stringNumber, 1);
-                    // mengupdate kondisi sekarang, mengkonversi 00 menjadi integer aslinya
-                    $number = intval($stringNumber);
-                    $stringNumber = strval($number);
-                    // return $pivot;
+                else {
+                    return $pivot = $pivot . ' ' . getTerbilang($number/1000) . ' ribu' . getTerbilang($number % 100);
                 }
-                $stringNumber = substr($stringNumber, 1);
-                $number = intval($stringNumber);
-                // mengupdate kondisi sekarang, mengkonversi 00 menjadi integer aslinya
-                $number = intval($stringNumber);
-                $stringNumber = strval($number);
                 
+            }elseif ($number > 1000 && $number % 1000 == 0 ) {
+                return $pivot = $pivot . ' ' . getTerbilang($number%1000) . ' ribu';
+            }else {
+                
+                return $pivot = $pivot . ' ' . getTerbilang($number % 1000);
             }
-
-            if ($level == 4){
-                // $stringNumber = strval($number);
-                if ($stringNumber[0] == '1') {
-                    $pivot = $pivot . 'se' . $determinant[13];
-                    $stringNumber = substr($stringNumber, 1);
-                    $number = intval($stringNumber);
-                    $stringNumber = strval($number);
-                }else {
-                    $pivot = $pivot . $determinant[strval($stringNumber[0])] . ' '. $determinant[13] . ' ';
-                    $stringNumber = substr($stringNumber, 1);
-                    $number = intval($stringNumber);
-                    $stringNumber = strval($number);
-                    
-                    // return $pivot . $stringNumber;
-                    // mengupdate kondisi sekarang, mengkonversi 00 menjadi integer aslinya
-                    
-                }
-            }
-
-            // level 5
-            // saat membuat level 5, perhatiken, agar dia bisa membedakan mana belasan ribu, dan mana puluhan ribu
-            // caranya cuman cek kepalanya
-            // kalo kepalanya 1, berarti, puluh ribu, kalo bukan, langsung aja cek di determinan
-
-            if($level == 5){
-                if ($stringNumber[0] == '1'){
-                    if ($stringNumber[1] == '1'){
-                        $pivot = $pivot . 'sebelas ribu';
-                        
-                        
-                    }else {
-                        $pivot = $pivot . $determinant[$stringNumber[1]] . 'belas ribu';
-                    }
-                }else {
-                    // cek untuk puluhan ribu
-                    if ($stringNumber[1] == '0'){
-                        $pivot = $pivot . $determinant[$stringNumber[0]] . 'puluh ribu';
-                    }else {
-                        $pivot = $pivot . $determinant[$stringNumber[0]] . 'puluh ' . $determinant[$stringNumber[1]] . ' ribu';
-                    }
-                }
-                // karerna dia sebelas ribu, maka potong angkanya 2 digit
-                $stringNumber = substr($stringNumber, 2);
-                $number = intval($stringNumber);
-                $stringNumber = strval($number);
-            }
-
-            if ($level == 6) {
-                // cek dia ju
-            }
-
-            if ($level == 1) {
-                $pivot = $pivot . $determinant[$number];
-                return $pivot;
-            }
-
+        }elseif ($number / 1000 > 11) {
+            return $pivot = $pivot . getTerbilang($number/1000) . ' ribu' . getTerbilang($number%1000); 
         }
-        return $pivot;
     }
 
     echo getTerbilang($ex);
