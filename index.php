7ad1b5c1575fc2b01162f35cb9e98bf6982c8ff7
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-15 18:53:08 +0600

    fix all algorithm dan berhasilll

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    $ex = 199099;
-    function getTerbilang($number){
+    $ex = 199857245678;
+    function getTerbilang($number)
+    {
         $pivot = '';
         $positition = 0;
         $level = strlen(strval($number));
@@ -16,63 +17,52 @@
             7 => 'tujuh',
             8 => 'delapan',
             9 => 'sembilan',
-            10 => 'puluh',
+            10 => 'sepuluh',
             11 => 'sebelas',
             12 => 'ratus',
-            13 => 'ribu' 
+            13 => 'ribu'
         ];
-        
-        
-        if($number < 12){
-            // cek apa die ade di antare 0-11
+    
+        // kondisi kurang dari 12
+        if ($number < 12) {
             return $pivot = $pivot . ' ' . $determinant[$number];
-        }elseif ($number > 11 && $number < 20 ) {
-            // jika lebih dari 11, maka cek  untuk belasan
-            return $pivot = $pivot . ' ' . ($determinant[$number-10]) . ' belas';
-        }elseif ($number > 19 && $number < 100) {
-            // untuk mengecek apakah dia memerlukan rekursi lagi untuk level puluhan
-            if ($number % 10 == 0) { // ini untuk ngecek yang habis pas tanpa sisa
-                return $pivot = $pivot . ' ' . $determinant[$number/10] . ' puluh';
-            }else {
-                // sedangkan baris ini untuk mengecek, apakah dia perlu rekursi lagi
-                return $pivot = $pivot . ' ' . $determinant[$number/10] . ' puluh' . getTerbilang($number % 10);
-            }     
-        }elseif ($number > 99 && $number < 1000) {
-            // belum bisa memasukkan seratusan biar dibaca seratus
-            if ($number / 100 > 0) {
-                if ($number / 100 == 1) { return $pivot = $pivot . ' ' . ' seratus'; }
-                return $pivot = $pivot . ' ' . getTerbilang($number/100) . ' ratus' . getTerbilang($number % 100);
-            }elseif ($number > 100 && $number % 100 == 0 ) {
-                return $pivot = $pivot . ' ' . getTerbilang($number%100) . ' ratus';
-            }else {
-                return $pivot = $pivot . ' ' . getTerbilang($number % 100);
-            }
-        }elseif ($number > 999 && $number / 1000 < 12) {
-            
-            if ($number / 1000 > 0) {
-                if ($number >= 1000 && $number <= 1999) {
-                    $pivot = $pivot . ' ' . ' seribu';
-                    if ($number % 1000 > 0){
-                        return $pivot = $pivot . ' ' . getTerbilang($number%1000);
-                    }else {
-                        return $pivot = $pivot . ' ' . getTerbilang($number/1000) . ' ribu' . getTerbilang($number % 100);
-                    }
-                }
-                else {
-                    return $pivot = $pivot . ' ' . getTerbilang($number/1000) . ' ribu' . getTerbilang($number % 100);
-                }
-                
-            }elseif ($number > 1000 && $number % 1000 == 0 ) {
-                return $pivot = $pivot . ' ' . getTerbilang($number%1000) . ' ribu';
-            }else {
-                
-                return $pivot = $pivot . ' ' . getTerbilang($number % 1000);
-            }
-        }elseif ($number / 1000 > 11) {
-            return $pivot = $pivot . getTerbilang($number/1000) . ' ribu' . getTerbilang($number%1000); 
         }
-    }
 
+        // kondisi lebih dari 12 -- belasan
+        if ($number > 11 && $number < 20) {
+            return $pivot = $pivot . ' ' . $determinant[$number-10] . ' belas';
+        }
+        // kondisi puluhan
+        if ($number > 19 && $number < 100) {
+            return $pivot = $pivot . ' ' . $determinant[$number/10] . ' puluh' . getTerbilang($number%10);
+        }
+        // kondisi ratusan
+        if ($number > 99 && $number < 1000) {
+            if (floor($number/100) == 1) {return $pivot = $pivot . ' ' . ' seratus' . getTerbilang($number%100);}
+            return $pivot = $pivot . ' ' . $determinant[$number/100] . ' ratus' . getTerbilang($number%100);
+        }
+        // kondisi ribuan - 
+        if ($number > 990 && $number < 1000000) {
+            if (floor($number/1000) == 1) {return $pivot = $pivot . ' ' . ' seribu' . getTerbilang($number%1000);}
+            return $pivot = $pivot . ' ' . getTerbilang($number/1000) . ' ribu' . getTerbilang($number%1000);
+        }
+
+        if ($number >= 1000000 && $number < 1000000000) {
+            // if (floor($number/1000000) == 1) {return $pivot = $pivot . ' ' . ' seribu' . getTerbilang($number%1000000);}
+            return $pivot = $pivot . ' ' . getTerbilang($number/1000000) . ' juta' . getTerbilang($number%1000000);
+        }
+
+        if ($number >= 1000000000 && $number < 1000000000000) {
+            if (floor($number/1000000) < 1000) {return 'asu'; return $pivot = $pivot . ' ' . getTerbilang(floor($number/1000000)) . ' juta' . getTerbilang($number%1000000);}
+            return $pivot = $pivot . ' ' . getTerbilang($number/1000000000) . ' milyar' . getTerbilang($number%1000000000);
+        }
+
+        // kondisi puluhan
+
+        // kondisi ratusan
+
+        // kondisi ribuan
+    }
     echo getTerbilang($ex);
 
 ?>
